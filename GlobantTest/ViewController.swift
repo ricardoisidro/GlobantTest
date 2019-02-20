847ba41bb65a74203fc82863f1349d4b32a8a3f4
ricardo <ricardo.isidro.86@gmail.com>
2019-02-20 10:41:49 -0600

    Initial Commit

---
+++ GlobantTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GlobantTest
+//
+//  Created by Ricardo Isidro on 2/20/19.
+//  Copyright © 2019 RicardoIsidro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
